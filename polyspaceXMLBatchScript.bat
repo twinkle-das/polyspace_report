e4fa76566c5b0ae2da66de6c724bd6a18467e550
twinkle-das <twinkledas086@gmail.com>
2021-06-11 19:11:31 +0530

    removed spaces from batch script

--- polyspaceXMLBatchScript.bat
+++ polyspaceXMLBatchScript.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set polyspace_log_file = %1
-set build_log_file = %2
-set output_file = %3
+set polyspace_log_file=%1
+set build_log_file=%2
+set output_file=%3
 
 python "polyspace.py" %polyspace_log_file% %build_log_file% %output_file%
 
