570a345b305335b42c62a8a92134bb1acd33dd6f
Twinkle <43482049+twinkle-das@users.noreply.github.com>
2021-07-12 16:55:45 +0530

    Add files via upload

--- polyspaceXML.bat
+++ polyspaceXML.bat
@@ -3,8 +3,9 @@
 set polyspace_log_file=%1
 set build_log_file=%2
 set threshold_properties_file=%3
-set output_file=%4
+set code_metric_file=%4
+set output_file=%5
 
-python "polyspaceXML.py" %polyspace_log_file% %build_log_file% %threshold_properties_file% %output_file%
+python "polyspaceXML.py" %polyspace_log_file% %build_log_file% %threshold_properties_file% %code_metric_file% %output_file%
 
 pause
